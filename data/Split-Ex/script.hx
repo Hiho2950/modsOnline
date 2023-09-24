8b9c0bfc434c7707e0efc969a37194c1fcf875ba
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-24 15:50:34 +0100

    idk

--- data/Split-Ex/script.hx
+++ data/Split-Ex/script.hx
@@ -30,11 +30,11 @@ function onCreate() {
     floor1.scrollFactor.set(0.75,0.75);
     floor1.scale.set(0.5, 0.5);
 
-    game.insert(game.members.indexOf(getLowestCharacterGroup()), sky);
-    game.insert(game.members.indexOf(getLowestCharacterGroup()), floor2);
-    game.insert(game.members.indexOf(getLowestCharacterGroup()), blu);
-    game.insert(game.members.indexOf(getLowestCharacterGroup()), mini);
-    game.insert(game.members.indexOf(getLowestCharacterGroup()), floor1);
+    add(sky);
+    add(floor2);
+    add(blu);
+    add(mini);
+    add(floor1);
 }
 function onSongStart() {
     game.boyfriend.x = 950;
@@ -43,25 +43,4 @@ function onSongStart() {
     game.gf.y = 240;
     game.dad.x = 150;
     game.dad.y = 189;
-}
-
-function getLowestCharacterGroup()
-{
-    var group:FlxSpriteGroup = game.gfGroup;
-    var pos:Int = game.members.indexOf(group);
-
-    var newPos:Int = game.members.indexOf(PlayState.instance.boyfriendGroup);
-    if(newPos < pos)
-    {
-        group = game.boyfriendGroup;
-        pos = newPos;
-    }
-    
-    newPos = game.members.indexOf(game.dadGroup);
-    if(newPos < pos)
-    {
-        group = game.dadGroup;
-        pos = newPos;
-    }
-    return group;
 }
