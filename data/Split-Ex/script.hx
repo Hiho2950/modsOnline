0b5562b98a867e0badf5887fecb3710559a80aab
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-24 14:45:07 +0100

    idk

--- data/Split-Ex/script.hx
+++ data/Split-Ex/script.hx
@@ -1,5 +1,6 @@
 import flixel.group.FlxSpriteGroup;
 
+function onSongStart() {
     var sky = new FlxSprite(-300, -60);
     setSpriteImage(sky, "BG1");
     sky.scrollFactor.set(0.5,0.5);
@@ -41,6 +42,7 @@ import flixel.group.FlxSpriteGroup;
     game.gf.y = 240;
     game.dad.x = 150;
     game.dad.y = 189;
+}
 
 function getLowestCharacterGroup():FlxSpriteGroup
 {
