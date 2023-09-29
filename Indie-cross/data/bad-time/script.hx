0d88b290b9e4ca6c20d34ab70d96a8708bf3e7c1
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-29 18:51:40 +0100

    idk

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -1,16 +1,16 @@
 function onSongStart() {
-    for (note in game.unspawnNotes) {
-        if (note.noteType == "BlueBoneNote") {
-            note.ignoreNote = true;
-            note.r = 0xFFFFFFFF;
-            note.g = 0xFF0000FF;
-            note.b = 0xFF0000FF;
+    for (i in game.unspawnNotes.length -1) {
+        if (game.unspawnNotes[i].noteType == "BlueBoneNote") {
+            game.unspawnNotes[i].unspawnNotes[i].ignoreNote = true;
+            game.unspawnNotes[i].r = 0xFFFFFFFF;
+            game.unspawnNotes[i].g = 0xFF0000FF;
+            game.unspawnNotes[i].b = 0xFF0000FF;
         }
-        if (note.noteType == "OrangeBoneNote") {
-            note.ignoreNote = false;
-            note.r = 0xFFFFFFFF;
-            note.g = 0xFFFFA500;
-            note.b = 0xFFFFA500;
+        if (game.unspawnNotes[i].noteType == "OrangeBoneNote") {
+            game.unspawnNotes[i].ignoreNote = false;
+            game.unspawnNotes[i].r = 0xFFFFFFFF;
+            game.unspawnNotes[i].g = 0xFFFFA500;
+            game.unspawnNotes[i].b = 0xFFFFA500;
         }
     }
 }
