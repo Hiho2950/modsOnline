b31c8d9aa9bf8991c3143c32b47b9f3df4c0e07c
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-29 16:49:44 +0100

    test for bad time

---
+++ Indie-cross/data/bad-time/script.hx
@@ -0,0 +1,24 @@
+function onCreate() {
+    for (note in game.unspawnNotes) {
+        if (note.noteType == "BlueBoneNote") {
+            note.ignoreNote = true;
+            note.r = 0xFFFFFFFF;
+            note.g = 0xFF0000FF;
+            note.b = 0xFF0000FF;
+        }
+        if (note.noteType == "OrangeBoneNote") {
+            note.ignoreNote = true;
+            note.r = 0xFFFFFFFF;
+            note.g = 0xFFFFA500;
+            note.b = 0xFFFFA500;
+        }
+    }
+}
+
+function goodNoteHit(note) {
+    if (note.noteType == "BlueBoneNote") geme.health -= 0.4;
+}
+
+function noteMiss(note) {
+    if (note.noteType == "OrangeBoneNote") geme.health -= 0.4;
+}
