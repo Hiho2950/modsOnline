3b678234ba10f05f715ce8e36308fcf1e50b2ff7
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-29 20:06:15 +0100

    fixing r variable

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -1,13 +1,13 @@
 function onSpawnNote(note) {
     if (note.noteType == "BlueBoneNote") {
         note.ignoreNote = true;
-        note.rgbShader.r = 0xFFFFFFFF;
+        note.rgbShader.r = 0xFF000000;
         note.rgbShader.g = 0xFF0000FF;
         note.rgbShader.b = 0xFF0000FF;
     }
     if (note.noteType == "OrangeBoneNote") {
         note.ignoreNote = false;
-        note.rgbShader.r = 0xFFFFFFFF;
+        note.rgbShader.r = 0xFF000000;
         note.rgbShader.g = 0xFFFFA500;
         note.rgbShader.b = 0xFFFFA500;
     }
