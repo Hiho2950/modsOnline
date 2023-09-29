d64d9ed6f4934adcd59e58eb749a35315132d497
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-29 19:05:55 +0100

    idk

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -1,17 +1,15 @@
-function opponentNoteHit() {
-    for (i in game.unspawnNotes.length -1) {
-        if (game.unspawnNotes[i].noteType == "BlueBoneNote") {
-            game.unspawnNotes[i].unspawnNotes[i].ignoreNote = true;
-            game.unspawnNotes[i].r = 0xFFFFFFFF;
-            game.unspawnNotes[i].g = 0xFF0000FF;
-            game.unspawnNotes[i].b = 0xFF0000FF;
-        }
-        if (game.unspawnNotes[i].noteType == "OrangeBoneNote") {
-            game.unspawnNotes[i].ignoreNote = false;
-            game.unspawnNotes[i].r = 0xFFFFFFFF;
-            game.unspawnNotes[i].g = 0xFFFFA500;
-            game.unspawnNotes[i].b = 0xFFFFA500;
-        }
+function onSpawnNote(note) {
+    if (note.noteType == "BlueBoneNote") {
+        note.unspawnNotes[i].ignoreNote = true;
+        note.r = 0xFFFFFFFF;
+        note.g = 0xFF0000FF;
+        note.b = 0xFF0000FF;
+    }
+    if (note.noteType == "OrangeBoneNote") {
+        note.ignoreNote = false;
+        note.r = 0xFFFFFFFF;
+        note.g = 0xFFFFA500;
+        note.b = 0xFFFFA500;
     }
 }
 
