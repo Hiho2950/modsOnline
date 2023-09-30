84f515fc6d24bf13a3c2ebac0ee05672037828d5
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-09-30 23:08:25 +0100

    adding healthbar for bad-time

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -34,3 +34,34 @@ function onUpdate(elapsed) {
         game.modchartSprites.get("BGSans").animation.play("bg1");
     }
 }
+
+function onCreatePost() {
+    addnullSprite("healthBarP2", game.healthBar.x, game.healthBar.y - 6.6);
+    game.modchartSprites.get("healthBarP2").cameras = [game.camHUD];
+    game.modchartSprites.get("healthBarP2").makeGraphic(game.healthBar.width,game.healthBar.height, 0xFFFF0000);
+    insert(4, game.modchartSprites.get("healthBarP2"));
+
+    addnullSprite("healthBarP1", game.healthBar.x, game.healthBar.y - 6.6);
+    game.modchartSprites.get("healthBarP1").cameras = [game.camHUD];
+    game.modchartSprites.get("healthBarP1").makeGraphic(game.healthBar.width / 2,game.healthBar.height, 0xFFFFFF00);
+    insert(5, game.modchartSprites.get("healthBarP1"));
+
+
+function onUpdate() {
+    game.modchartSprites.get("healthBarP2").x = game.healthBar.x;
+    game.modchartSprites.get("healthBarP2").y = game.healthBar.y - 6.6;
+    game.modchartSprites.get("healthBarP1").x = game.healthBar.x;
+    game.modchartSprites.get("healthBarP1").y = game.healthBar.y - 6.6;
+
+    if (game.health <= 2) {
+        game.modchartSprites.get("healthBarP2").scale.set(game.health, game.healthBar.scale.y);
+        game.modchartSprites.get("healthBarP1").scale.set(game.healthBar.scale.x, game.healthBar.y);
+    }
+
+    game.modchartSprites.get("healthBarP2").width = game.healthBar.width;
+    game.modchartSprites.get("healthBarP2").height = game.healthBar.height;
+
+    game.iconP1.alpha = 0;
+    game.iconP2.alpha = 0;
+    game.healthBar.flipX = true;
+}
