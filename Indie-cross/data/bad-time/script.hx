9cdb1cfa8574657d5f3dbb6a9050ec4db296a8ea
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-10-08 21:28:26 +0100

    idk

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -36,10 +36,10 @@ function onUpdate(elapsed) {
         game.modchartSprites.get("BGSans").animation.play("bg1");
     }
 }
-
+importLibrary("flixel.ui.FlxBar");
+var bar:FlxBar
 function onCreatePost() {
-    importLibrary("flixel.ui.FlxBar");
-    var bar = new FlxBar(game.healthBar.x, game.heathBar.y, LEFT_TO_RIGHT, game.healthBar.width, game.healthBar.height, null, "", 0, 2, false);
+    bar = new FlxBar(game.healthBar.x, game.heathBar.y, LEFT_TO_RIGHT, game.healthBar.width, game.healthBar.height, null, "", 0, 2, false);
     bar.createFilledBar(0xFFFF0000, 0xFFFFFF00);
     insert(4, bar);
 }
