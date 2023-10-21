138f2f5b26b4336a7078dcf5ffc40af212e8f8ee
Hiho2950 <109638489+Hiho2950@users.noreply.github.com>
2023-10-21 02:40:26 +0100

    Update script.hx

--- Indie-cross/data/bad-time/script.hx
+++ Indie-cross/data/bad-time/script.hx
@@ -41,6 +41,7 @@ importLibrary("flixel.ui.FlxBar");
 importLibrary("flixel.ui.FlxBar.FlxBarFillDirection");
 var bar = new FlxBar(100, 100, null, 720, 50, null, "", 0, 2, false);
 function onCreatePost() {
+    bar.cameras = [game.camHUD];
     bar.createFilledBar(0xFFFF0000, 0xFFFFFF00);
     insert(4, bar);
 }
